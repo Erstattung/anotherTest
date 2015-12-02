15055464ae7f4710d54d8a2c5ee0bbb1fb2304ac
echerkas <dark_lady_666@mail.ru>
2015-12-03 02:33:36 +0300

    all

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ * Created by echerkas on 03.12.2015.
+ */
+public class Main {
+    public static void main (String [] args){
+        String s = "eee";
+        System.out.print(s);
+    }
+}
